838981876618ba5b74ae0d408f3b5fbabc0e814d
Soma <luis.vc@culiacan.tecnm.mx>
2024-12-18 19:12:40 -0700

    Agregar archivos de proyecto.

---
+++ TodoApi/Models/NuevoPedidoEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace Eventos.Models
+{
+    public class NuevoPedidoEvent
+    {
+        public Guid PedidoId { get; set; }
+        public string ClienteId { get; set; }
+        public List<string> Productos { get; set; }
+        public decimal Total { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
